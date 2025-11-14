51771de00644d8ca3d99e112d655a49321c25a3c
Pinapelz <yukais@pinapelz.com>
2025-11-14 02:33:27 -0800

    post: mech-luddite

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig, sharpImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import remarkGfm from 'remark-gfm';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.pinapelz.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx({
+    remarkPlugins: [remarkGfm],
+  }), sitemap(), react()],
   image:{
     service: sharpImageService(),
   }
